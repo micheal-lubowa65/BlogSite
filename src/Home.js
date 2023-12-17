0c63978d861569b568ef9e2d09a241ff9ef8be3c
micheal-lubowa65 <lubowa65@gmail.com>
2023-12-17 13:12:18 +0300

    third commit

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
 
@@ -11,15 +12,7 @@ const Home = () => {
 
     return ( 
         <div className="Home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p> <br />
-
-
-                </div>
-
-            ))}
+            <BlogList blogs = {blogs} title="All Blogs"/>
         
         </div>
 
