ea4aab7c3e68326334a84cc541c8d292ff89236b
micheal-lubowa65 <lubowa65@gmail.com>
2023-12-17 12:50:23 +0300

    second commit

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,27 @@
-const Home = () => {
-    return ( 
-<div className="Home">
-    <h1>Home Page</h1>
+import { useState } from "react";
 
-</div>
+const Home = () => {
+
+        const[blogs, setBlogs] = useState([
+
+            {title: 'My New Website', body: '    Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita voluptates quia saepe quae impedit obcaecati nihil, quo, distinctio ab reprehenderit cupiditate soluta a voluptatem tempora similique repudiandae blanditiis odit nobis?', author: 'mario', id: 1},
+            {title: 'Welcome party!', body: '    Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita voluptates quia saepe quae impedit obcaecati nihil, quo, distinctio ab reprehenderit cupiditate soluta a voluptatem tempora similique repudiandae blanditiis odit nobis?', author: 'luigi', id: 2},
+            {title: 'Nature', body: '    Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita voluptates quia saepe quae impedit obcaecati nihil, quo, distinctio ab reprehenderit cupiditate soluta a voluptatem tempora similique repudiandae blanditiis odit nobis?', author: 'daisy', id: 3}
+        ])
+
+    return ( 
+        <div className="Home">
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p> <br />
+
+
+                </div>
+
+            ))}
+        
+        </div>
 
      );
 }
