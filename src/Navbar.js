567f679492512b8e6aec25aa4dc5c5313164f4dc
micheal-lubowa65 <lubowa65@gmail.com>
2023-12-17 11:23:53 +0300

    First commit

---
+++ src/Navbar.js
@@ -0,0 +1,18 @@
+const Navbar = () => {
+    return (  
+
+        <nav className="navbar">
+            <h1>Dojo Blog Nav</h1>
+
+            <div className="links">
+                    <a href="/">Home</a>
+                    <a href="/create">Blog</a>
+
+            </div>
+          
+        </nav>
+
+    );
+}
+ 
+export default Navbar;
