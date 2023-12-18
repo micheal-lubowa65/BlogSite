d908032d62cbffbc95dab1dd6dea565fc84c87b8
micheal-lubowa65 <lubowa65@gmail.com>
2023-12-18 18:29:48 +0300

    SIXTH COMMIT

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,20 @@
+import { Link } from "react-router-dom/cjs/react-router-dom";
+   
 const Navbar = () => {
+   
     return (  
 
         <nav className="navbar">
             <h1>Dojo Blog Nav</h1>
 
             <div className="links">
-                    <a href="/">Home</a>
-                    <a href="/create">Blog</a>
+                    <Link to="/">Home</Link>
+                    <Link to="/create">  New Blog</Link>
 
             </div>
           
         </nav>
-
+ 
     );
 }
  
